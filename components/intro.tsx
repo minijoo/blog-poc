46bef488ebff5974b2c0d087ab8e2dd5756b8a83
Joo Kang <minijoo@gmail.com>
2023-10-05 17:59:33 -0400

    splash

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,7 +4,7 @@ const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        <div className="my-0 mx-auto block md:inline-block md:w-32 md:h-32 w-20 h-20 relative">
+        <div className="text-shadow my-0 mx-auto block md:inline-block md:w-32 md:h-32 w-20 h-20 relative">
           <Image src="/favicon/safari-pinned-tab.svg" alt="logo" fill />
         </div>{" "}
         Jordy's Blog.
--- components/layout.tsx
+++ components/layout.tsx
@@ -1,13 +1,52 @@
+import { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
 import Alert from "./alert";
 import Footer from "./footer";
 import Meta from "./meta";
 
+const COOKIE_NAME = "entered-jordys-site";
 type Props = {
   preview?: boolean;
   children: React.ReactNode;
 };
 
+const onEntry = (hasEntered) => {
+  // send counter update to api
+  // alert(hasEntered ? "you already entered" : "you are being entered");
+};
+
 const Layout = ({ preview, children }: Props) => {
+  const [hasEntered, setHasEntered] = useState(true);
+  const [cookies, setCookie] = useCookies([COOKIE_NAME]);
+  useEffect(() => {
+    setHasEntered(cookies[COOKIE_NAME] === 1);
+  }, [cookies]);
+  if (!hasEntered) {
+    return (
+      <>
+        <div className="text-white fixed h-full w-full bg-black z-50 place-content-center flex items-center">
+          <div className="grid grid-rows-2">
+            <div className="row-span-1">
+              <div
+                className="text-3xl py-1 px-2 hover:bg-white hover:text-black"
+                onClick={() => {
+                  setHasEntered(true);
+                  setCookie(COOKIE_NAME, 1, { maxAge: 60 * 24 });
+                  onEntry(hasEntered);
+                }}
+              >
+                Click to Enter
+              </div>
+            </div>
+            <div className="row-span-1 text-center">
+              <small>This site uses cookies.</small>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
       <Meta />
