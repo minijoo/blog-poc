3035a645eaa8f274bb924339bcec0fccf0124a3b
Joo Kang <minijoo@gmail.com>
2023-10-15 03:35:56 -0400

    img url fix

--- galleries/pixel/day2.ts
+++ galleries/pixel/day2.ts
@@ -3,82 +3,82 @@ import { mapGalleryToSlides } from "../../lib/utils";
 
 const galleryItems: GalleryItem[] = [
   {
-    path: "/assets/blog/temp/pixel1.jpg",
+    path: "https://d1goytf13un2gh.cloudfront.net/assets/pixel/pixel1.jpg",
     height: 1080,
     width: 800,
   },
   {
-    path: "/assets/blog/temp/pixel2.jpg",
+    path: "https://d1goytf13un2gh.cloudfront.net/assets/pixel/pixel2.jpg",
     height: 1080,
     width: 800,
   },
   {
-    path: "/assets/blog/temp/pixel3.jpg",
+    path: "https://d1goytf13un2gh.cloudfront.net/assets/pixel/pixel3.jpg",
     height: 1080,
     width: 800,
   },
   {
-    path: "/assets/blog/temp/pixel4.jpg",
+    path: "https://d1goytf13un2gh.cloudfront.net/assets/pixel/pixel4.jpg",
     height: 1080,
     width: 800,
   },
   {
-    path: "/assets/blog/temp/pixel5.jpg",
+    path: "https://d1goytf13un2gh.cloudfront.net/assets/pixel/pixel5.jpg",
     height: 1080,
     width: 800,
   },
   {
-    path: "/assets/blog/temp/pixel6.jpg",
+    path: "https://d1goytf13un2gh.cloudfront.net/assets/pixel/pixel6.jpg",
     height: 1080,
     width: 800,
   },
   {
-    path: "/assets/blog/temp/pixel7.jpg",
+    path: "https://d1goytf13un2gh.cloudfront.net/assets/pixel/pixel7.jpg",
     height: 1080,
     width: 800,
   },
   {
-    path: "/assets/blog/temp/pixel8.jpg",
+    path: "https://d1goytf13un2gh.cloudfront.net/assets/pixel/pixel8.jpg",
     height: 1080,
     width: 800,
   },
   {
-    path: "/assets/blog/temp/pixel9.jpg",
+    path: "https://d1goytf13un2gh.cloudfront.net/assets/pixel/pixel9.jpg",
     height: 1080,
     width: 800,
   },
   {
     type: "video",
-    path: "/assets/blog/temp/pixel10_cover.png",
+    path: "https://d1goytf13un2gh.cloudfront.net/assets/pixel/pixel10_cover.png",
     video: {
-      path: "/assets/blog/temp/pixel10.mp4",
+      path: "https://d1goytf13un2gh.cloudfront.net/assets/pixel/pixel10.mp4",
       type: "video/mp4",
     },
     height: 1008,
     width: 567,
   },
   {
-    path: "/assets/blog/temp/pixel11.jpg",
+    path: "https://d1goytf13un2gh.cloudfront.net/assets/pixel/pixel11.jpg",
     height: 1080,
     width: 800,
   },
   {
-    path: "/assets/blog/temp/pixel12.jpg",
+    path: "https://d1goytf13un2gh.cloudfront.net/assets/pixel/pixel12.jpg",
     height: 800,
     width: 1080,
   },
   {
-    path: "/assets/blog/temp/pixel13.jpg",
+    path: "https://d1goytf13un2gh.cloudfront.net/assets/pixel/pixel13.jpg",
     height: 800,
     width: 1080,
   },
   {
-    path: "/assets/blog/temp/pixel14.jpg",
+    path: "https://d1goytf13un2gh.cloudfront.net/assets/pixel/pixel14.jpg",
     height: 1080,
     width: 800,
   },
   {
-    path: "/assets/blog/temp/pixel15.jpg",
+    path: "https://d1goytf13un2gh.cloudfront.net/assets/pixel/pixel15.jpg",
     height: 1080,
     width: 800,
   },
