94b39039ecc4f29ace715a9a31295f2833643c99
Joo Kang <minijoo@gmail.com>
2024-07-08 12:35:20 +0900

    cover image size tweak

--- components/more-stories.tsx
+++ components/more-stories.tsx
@@ -56,22 +56,24 @@ const MoreStories = ({ posts }: Props) => {
       <Header />
       <div className="w-[200vw] text-center left-1/2 relative -translate-x-1/2 mx-auto">
         <div
-          className="md:px-10 px-2 w-screen absolute left-1/2 `-translate-x-1/2"
+          className="md:px-10 w-screen absolute left-1/2 `-translate-x-1/2"
           style={{
             transform: `translateX(${
               -1 * xTransC - p_width / 2
             }rem) translateY(${radius - yTransC}rem)`,
           }}
         >
-          <PostPreview
-            key={posts[0].slug}
-            title={posts[0].title}
-            coverImage={posts[0].coverImage}
-            date={posts[0].date}
-            author={posts[0].author}
-            slug={posts[0].slug}
-            excerpt={posts[0].excerpt}
-          />
+          <div className="px-2">
+            <PostPreview
+              key={posts[0].slug}
+              title={posts[0].title}
+              coverImage={posts[0].coverImage}
+              date={posts[0].date}
+              author={posts[0].author}
+              slug={posts[0].slug}
+              excerpt={posts[0].excerpt}
+            />
+          </div>
 
           <CoverImage
             title={posts[0].title}
@@ -86,17 +88,19 @@ const MoreStories = ({ posts }: Props) => {
               radius - yTransA
             }rem)`,
           }}
-          className="h-40 md:px-10 px-2 w-screen absolute left-1/2 -translate-x-1/2"
+          className="h-40 md:px-10 w-screen absolute left-1/2 -translate-x-1/2"
         >
-          <PostPreview
-            key={posts[1].slug}
-            title={posts[1].title}
-            coverImage={posts[1].coverImage}
-            date={posts[1].date}
-            author={posts[1].author}
-            slug={posts[1].slug}
-            excerpt={posts[1].excerpt}
-          />
+          <div className="px-2">
+            <PostPreview
+              key={posts[1].slug}
+              title={posts[1].title}
+              coverImage={posts[1].coverImage}
+              date={posts[1].date}
+              author={posts[1].author}
+              slug={posts[1].slug}
+              excerpt={posts[1].excerpt}
+            />
+          </div>
           <CoverImage
             title={posts[1].title}
             src={posts[1].coverImage}
@@ -109,17 +113,19 @@ const MoreStories = ({ posts }: Props) => {
               radius + yTransB
             }rem)`,
           }}
-          className="h-40 md:px-10 px-2 w-screen absolute left-1/2 -translate-x-1/2"
+          className="h-40 md:px-10 w-screen absolute left-1/2 -translate-x-1/2"
         >
-          <PostPreview
-            key={posts[2].slug}
-            title={posts[2].title}
-            coverImage={posts[2].coverImage}
-            date={posts[2].date}
-            author={posts[2].author}
-            slug={posts[2].slug}
-            excerpt={posts[2].excerpt}
-          />
+          <div className="px-2">
+            <PostPreview
+              key={posts[2].slug}
+              title={posts[2].title}
+              coverImage={posts[2].coverImage}
+              date={posts[2].date}
+              author={posts[2].author}
+              slug={posts[2].slug}
+              excerpt={posts[2].excerpt}
+            />
+          </div>
           <CoverImage
             title={posts[2].title}
             src={posts[2].coverImage}
@@ -132,17 +138,19 @@ const MoreStories = ({ posts }: Props) => {
               radius + yTransC
             }rem)`,
           }}
-          className="h-40 md:px-10 px-2 w-screen absolute left-1/2 -translate-x-1/2"
+          className="h-40 md:px-10 w-screen absolute left-1/2 -translate-x-1/2"
         >
-          <PostPreview
-            key={posts[3].slug}
-            title={posts[3].title}
-            coverImage={posts[3].coverImage}
-            date={posts[3].date}
-            author={posts[3].author}
-            slug={posts[3].slug}
-            excerpt={posts[3].excerpt}
-          />
+          <div className="px-2">
+            <PostPreview
+              key={posts[3].slug}
+              title={posts[3].title}
+              coverImage={posts[3].coverImage}
+              date={posts[3].date}
+              author={posts[3].author}
+              slug={posts[3].slug}
+              excerpt={posts[3].excerpt}
+            />
+          </div>
           <CoverImage
             title={posts[3].title}
             src={posts[3].coverImage}
@@ -155,17 +163,19 @@ const MoreStories = ({ posts }: Props) => {
               -1 * xTransA - p_width / 2
             }rem) translateY(${radius + yTransA}rem)`,
           }}
-          className="h-40 md:px-10 px-2 w-screen absolute left-1/2 -translate-x-1/2"
+          className="h-40 md:px-10 w-screen absolute left-1/2 -translate-x-1/2"
         >
-          <PostPreview
-            key={posts[4].slug}
-            title={posts[4].title}
-            coverImage={posts[4].coverImage}
-            date={posts[4].date}
-            author={posts[4].author}
-            slug={posts[4].slug}
-            excerpt={posts[4].excerpt}
-          />
+          <div className="px-2">
+            <PostPreview
+              key={posts[4].slug}
+              title={posts[4].title}
+              coverImage={posts[4].coverImage}
+              date={posts[4].date}
+              author={posts[4].author}
+              slug={posts[4].slug}
+              excerpt={posts[4].excerpt}
+            />
+          </div>
           <CoverImage
             title={posts[4].title}
             src={posts[4].coverImage}
@@ -178,17 +188,19 @@ const MoreStories = ({ posts }: Props) => {
               -1 * xTransB - p_width / 2
             }rem) translateY(${radius - yTransB}rem)`,
           }}
-          className="h-40 md:px-10 px-2 w-screen absolute left-1/2 -translate-x-1/2"
+          className="h-40 md:px-10 w-screen absolute left-1/2 -translate-x-1/2"
         >
-          <PostPreview
-            key={posts[5].slug}
-            title={posts[5].title}
-            coverImage={posts[5].coverImage}
-            date={posts[5].date}
-            author={posts[5].author}
-            slug={posts[5].slug}
-            excerpt={posts[5].excerpt}
-          />
+          <div className="px-2">
+            <PostPreview
+              key={posts[5].slug}
+              title={posts[5].title}
+              coverImage={posts[5].coverImage}
+              date={posts[5].date}
+              author={posts[5].author}
+              slug={posts[5].slug}
+              excerpt={posts[5].excerpt}
+            />
+          </div>
           <CoverImage
             title={posts[5].title}
             src={posts[5].coverImage}
