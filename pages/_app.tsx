9f9ba054a3edf4b919fcc3227546acb8f1d534e5
Joo Kang <minijoo@gmail.com>
2024-08-13 21:55:14 -0400

    cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,6 @@ import "yet-another-react-lightbox/plugins/thumbnails.css";
 import "yet-another-react-lightbox/styles.css";
 import "../styles/index.css";
 import { Crimson_Text } from "next/font/google";
-import { useLayoutEffect, useRef } from "react";
-import {
-  getMainScrollHeight,
-  setMainScrollHeight,
-} from "../components/height-observer";
 
 const crimson = Crimson_Text({
   weight: ["400"],
@@ -16,14 +11,8 @@ const crimson = Crimson_Text({
 });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const ref = useRef(null);
-
-  useLayoutEffect(() => {
-    setMainScrollHeight(ref.current.scrollHeight);
-    // Only place/way I know /how for getting scrollable height of body/html
-  });
   return (
-    <main className={`${crimson.variable} font-serif`} ref={ref}>
+    <main className={`${crimson.variable} font-serif`}>
       <Component {...pageProps} />
     </main>
   );
