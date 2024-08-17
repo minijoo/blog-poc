2a7504336d239854950f9cc3c8f001d014cd6899
Joo Kang <minijoo@gmail.com>
2024-08-17 14:07:45 -0400

    test2

--- components/cover-image-for-post.tsx
+++ components/cover-image-for-post.tsx
@@ -13,18 +13,20 @@ const CoverImage = ({ title, src, slug }: Props) => {
   const image = (
     <div>
       <div
-        className="w-full h-36 md:h-48 bg-cover bg-center"
+        className="w-full bg-cover bg-center"
         style={{
           backgroundImage: `url(${src})`,
         }}
       >
-        <div className="h-full backdrop-blur-sm bg-white/50 px-5 md:px-10 py-6 md:py-12">
-          {/* <Textfit
+        <div className="h-full backdrop-blur-sm bg-white/50 px-5 md:px-10">
+          <Textfit
+            mode="multi"
             max={1000}
-            className="h-full text-center tracking-tighter flex items-center place-content-center"
-          > */}
-          {title}
-          {/* </Textfit> */}
+            style={{ height: "144px" }}
+            className="text-center tracking-tighter flex place-items-center"
+          >
+            {title}
+          </Textfit>
         </div>
       </div>
     </div>
