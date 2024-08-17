a6ed19c0536db07bda8e7a07e95bea9b3cb778b3
Joo Kang <minijoo@gmail.com>
2024-08-17 19:21:54 -0400

    contact page post page new footer

--- pages/download.tsx
+++ pages/download.tsx
@@ -1,28 +1,27 @@
-import { useState } from "react";
+import { useLayoutEffect, useRef, useState } from "react";
 import cn from "classnames";
 import hhPhotos from "../galleries/hh-photos";
 import PhotoGallery from "../components/photo-gallery";
+import TextFit from "textfit";
 
 const url0 =
   "https://public--asia.s3.ap-northeast-2.amazonaws.com/JYK+Scan+Images.zip";
 
 export default function DownloadPage() {
+  const ref = useRef();
+
+  useLayoutEffect(() => {
+    console.log(ref.current);
+    TextFit(ref.current, { multiLine: true });
+  }, []);
+
   const [showing, setShowing] = useState(false);
+
   return (
     <>
-      <div className="p-5">
-        <button
-          className={cn(
-            "m-5 bg-blue-500 hover:bg-blue-700 rounded py-2 px-4 text-white",
-            { hidden: showing }
-          )}
-          onClick={() => setShowing(true)}
-        >
-          Show photos
-        </button>
-        <div className={cn("", { hidden: !showing })}>
-          <PhotoGallery slides={hhPhotos} />
-        </div>
+      <div ref={ref} className="w-[60vw] h-32 text-justify">
+        this is a piece of text that is I mean this works? what does multiline
+        do exactly
       </div>
     </>
   );
