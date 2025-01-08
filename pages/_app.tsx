7c5cb45456dd259393b19507d2107c62236496e6
Joo Kang <minijoo@gmail.com>
2025-01-08 12:40:25 -0500

    font use cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,16 @@ import { AppProps } from "next/app";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 import "yet-another-react-lightbox/styles.css";
 import "../styles/index.css";
-import { Crimson_Text } from "next/font/google";
+import { Crimson_Pro } from "next/font/google";
 
-const crimson = Crimson_Text({
-  weight: ["400"],
+const crimson = Crimson_Pro({
   subsets: ["latin"],
-  variable: "--font-crimson",
+  display: "swap",
 });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <main className={`${crimson.variable} font-serif`}>
+    <main className={`${crimson.className}`}>
       <Component {...pageProps} />
     </main>
   );
