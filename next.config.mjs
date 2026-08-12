39cdd988bd4ee58e3b22cff6de8c7520c9a01c10
jordy <minijoo@gmail.com>
2026-08-12 10:45:11 -0400

    contact up

--- next.config.mjs
+++ next.config.mjs
@@ -3,56 +3,57 @@
 import bundleAnalyzer from "@next/bundle-analyzer";
 
 const configuration = {
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    return config;
-  },
-  // Support MDX files as pages:
-  pageExtensions: ["tsx", "ts", "jsx", "js"],
-  // Support loading `.md`, `.mdx`:
-  // webpack(config, options) {
-  // config.module.rules.push({
-  //   test: /\.mdx?$/,
-  //   use: [
-  //     // The default `babel-loader` used by Next:
-  //     options.defaultLoaders.babel,
-  //     {
-  //       loader: "@mdx-js/loader",
-  //       /** @type {import('@mdx-js/loader').Options} */
-  //       options: {
-  //         providerImportSource: "@mdx-js/react",
-  //         /* jsxImportSource: …, otherOptions… */
-  //       },
-  //     },
-  //   ],
-  // });
-  // return config;
-  // },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "jordysbucket.s3.us-east-1.amazonaws.com",
-        port: "",
-        pathname: "/public/assets/**",
-      },
-      {
-        protocol: "https",
-        hostname: "d1goytf13un2gh.cloudfront.net",
-        port: "",
-        pathname: "/assets/**",
-      },
-    ],
-    dangerouslyAllowSVG: true,
-    contentDispositionType: "attachment",
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-  },
+	turbopack: {},
+	webpack: (
+		config,
+		{ buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+	) => {
+		return config;
+	},
+	// Support MDX files as pages:
+	pageExtensions: ["tsx", "ts", "jsx", "js"],
+	// Support loading `.md`, `.mdx`:
+	// webpack(config, options) {
+	// config.module.rules.push({
+	//   test: /\.mdx?$/,
+	//   use: [
+	//     // The default `babel-loader` used by Next:
+	//     options.defaultLoaders.babel,
+	//     {
+	//       loader: "@mdx-js/loader",
+	//       /** @type {import('@mdx-js/loader').Options} */
+	//       options: {
+	//         providerImportSource: "@mdx-js/react",
+	//         /* jsxImportSource: …, otherOptions… */
+	//       },
+	//     },
+	//   ],
+	// });
+	// return config;
+	// },
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "jordysbucket.s3.us-east-1.amazonaws.com",
+				port: "",
+				pathname: "/public/assets/**",
+			},
+			{
+				protocol: "https",
+				hostname: "d1goytf13un2gh.cloudfront.net",
+				port: "",
+				pathname: "/assets/**",
+			},
+		],
+		dangerouslyAllowSVG: true,
+		contentDispositionType: "attachment",
+		contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+	},
 };
 
 const withBundleAnalyzer = bundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
+	enabled: process.env.ANALYZE === "true",
 });
 
 export default withBundleAnalyzer(configuration);
